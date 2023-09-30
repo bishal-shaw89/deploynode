51aee92ba37b95444fd5c9ffe184ae6c4f0e2844
Bishal-Tier5 <bishal.shaw@tier5.in>
2023-09-30 22:33:21 +0530

    deploy node

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+    res.send('Hello Test!')
+})
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
